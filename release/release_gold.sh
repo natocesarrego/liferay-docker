a872427898a55e25ec053a6c2871c5423747c8f2
Lucas Miranda <lucas.miranda@liferay.com>
2024-05-03 13:16:26 -0300

    LPD-24644 Add new API call to create a ref needed to publish a new tag

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -115,8 +115,8 @@ function tag_release {
 			--data-raw '
 				{
 					"message": "",
-					"object": "'$(lc_get_property release-data/release.properties git.hash.liferay-portal-ee)'",
-					"tag": "'${LIFERAY_RELEASE_VERSION}'",
+					"object": "'$(lc_get_property "${_PROJECTS_DIR}"/liferay-portal-ee/release.properties release.tool.sha)'",
+					"tag": "'"${LIFERAY_RELEASE_VERSION}"'",
 					"type": "commit"
 				}' \
 			--fail \
@@ -129,6 +129,28 @@ function tag_release {
 			--silent)
 	then
 		lc_log ERROR "Unable to tag release."
+
+		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	fi
+
+	if (! curl \
+			"https://api.github.com/repos/liferay/${repository}/git/refs" \
+			--data-raw '
+				{
+					"message": "",
+					"ref": "refs/tags/'"${LIFERAY_RELEASE_VERSION}"'",
+					"sha": "'$(lc_get_property "${_PROJECTS_DIR}"/liferay-portal-ee/release.properties release.tool.sha)'"
+				}' \
+			--fail \
+			--header "Accept: application/vnd.github+json" \
+			--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
+			--header "X-GitHub-Api-Version: 2022-11-28" \
+			--max-time 10 \
+			--request POST \
+			--retry 3 \
+			--silent)
+	then
+		echo "Unable to tag release."
 
 		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 	fi
