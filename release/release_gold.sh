93a371d030d162797f5bd72230ae3ea689920399
Pedro Malta <pedro.malta@liferay.com>
2024-08-30 09:51:50 -0300

    LPD-35241 Preparing release_gold.sh for tests

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -63,6 +63,11 @@ function commit_to_branch {
 }
 
 function main {
+	if [[ " ${@} " =~ " --test " ]]
+	then
+		return
+	fi
+
 	check_usage
 
 	check_supported_versions
@@ -393,4 +398,4 @@ function update_release_info_date {
 	fi
 }
 
-main
+main "${@}"
