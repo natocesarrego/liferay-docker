2bd957f73c3351d48beaff414ca024c880fe0aaa
Lucas Miranda <lucas.miranda@liferay.com>
2024-09-18 18:28:03 -0300

    LPD-24037 Download 2024.q2.6 bundle to test generate_distro_jar

--- release/test_promotion.sh
+++ release/test_promotion.sh
@@ -32,11 +32,17 @@ function set_up {
 	fi
 
 	export LIFERAY_RELEASE_PRODUCT_NAME="dxp"
-	export LIFERAY_RELEASE_VERSION="2024.q2.0"
+	export LIFERAY_RELEASE_VERSION="2024.q2.6"
 
 	export _BUILD_DIR="${_RELEASE_ROOT_DIR}/release-data/build"
 
-	export _BUNDLES_DIR="${_RELEASE_ROOT_DIR}/release-data/dev/projects/bundles"
+	lc_cd "${_RELEASE_ROOT_DIR}/test-dependencies"
+
+	wget https://releases-cdn.liferay.com/dxp/2024.q2.6/liferay-dxp-tomcat-2024.q2.6-1721635298.zip &> /dev/null
+
+	unzip -q liferay-dxp-tomcat-2024.q2.6-1721635298.zip
+
+	export _BUNDLES_DIR="${_RELEASE_ROOT_DIR}/test-dependencies/liferay-dxp"
 
 	export _PRODUCT_VERSION="${LIFERAY_RELEASE_VERSION}"
 
@@ -50,7 +56,11 @@ function set_up {
 }
 
 function tear_down {
+	pgrep --full --list-name "${_BUNDLES_DIR}" | awk '{print $1}' | xargs --no-run-if-empty kill -9
+
+	rm -fr "${_BUNDLES_DIR}"
 	rm -fr "${_RELEASE_ROOT_DIR}/release-data/build/boms"
+	rm -f "${_RELEASE_ROOT_DIR}/test-dependencies/liferay-dxp-tomcat-2024.q2.6-1721635298.zip"
 
 	unset LIFERAY_RELEASE_PRODUCT_NAME
 	unset LIFERAY_RELEASE_VERSION
