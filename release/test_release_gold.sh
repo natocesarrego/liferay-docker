7a21eca81ac37823dfc57b58c95901ae0d1da782
Renato Rego <renato.rego@liferay.com>
2024-09-20 13:28:27 -0300

    LPD-35241 Optimize tests

--- release/test_release_gold.sh
+++ release/test_release_gold.sh
@@ -55,41 +55,22 @@ function test_invoke_github_api_post {
 }
 
 function test_update_release_info_date {
-	_PRODUCT_VERSION="2024.q2.11"
+	_test_update_release_info_date "2024.q2.11" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	_test_update_release_info_date "2024.q2.12" "${LIFERAY_COMMON_EXIT_CODE_OK}"
+	_test_update_release_info_date "2024.q3.0" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	_test_update_release_info_date "7.3.10-u36" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	_test_update_release_info_date "7.4.13-u101" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	_test_update_release_info_date "7.4.3.125-ga125" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+}
+
+function _test_update_release_info_date {
+	_PRODUCT_VERSION="${1}"
+
+	echo -e "Running _test_update_release_info_date for ${_PRODUCT_VERSION}\n"
 
 	update_release_info_date --test >/dev/null
 
-	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
-
-	_PRODUCT_VERSION="2024.q2.12"
-
-	update_release_info_date --test >/dev/null
-
-	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_OK}"
-
-	_PRODUCT_VERSION="2024.q3.0"
-
-	update_release_info_date --test >/dev/null
-
-	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
-
-	_PRODUCT_VERSION="7.3.10-u36"
-
-	update_release_info_date --test >/dev/null
-
-	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
-
-	_PRODUCT_VERSION="7.4.13-u101"
-
-	update_release_info_date --test >/dev/null
-
-	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
-
-	_PRODUCT_VERSION="7.4.3.125-ga125"
-
-	update_release_info_date --test >/dev/null
-
-	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	assert_equals "${?}" "${2}"
 }
 
 main
