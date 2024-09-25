287c0ea0b7cdfa0359e72c898e6b0558c452548e
Renato Rego <renato.rego@liferay.com>
2024-09-25 18:33:49 -0300

    LPD-36948 SF

--- release/test_git.sh
+++ release/test_git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+source ../_test_common.sh
 source _git.sh
 source _liferay_common.sh
-source ../_test_common.sh
 
 function main {
 	set_up
@@ -18,13 +18,12 @@ function main {
 }
 
 function set_up {
-	export _PRODUCT_VERSION="2024.q2.0"
-	export _RELEASE_ROOT_DIR="${PWD}"
 	export LIFERAY_RELEASE_PRODUCT_NAME="dxp"
+	export _PRODUCT_VERSION="2024.q2.0"
+
+	export _RELEASE_ROOT_DIR="${PWD}"
 
 	export _PROJECTS_DIR="${_RELEASE_ROOT_DIR}"/../..
-
-	export _BUILD_DIR="${_PROJECTS_DIR}/liferay-portal-ee"
 
 	if [ ! -d "${_PROJECTS_DIR}/liferay-portal-ee" ]
 	then
@@ -33,22 +32,24 @@ function set_up {
 		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 	fi
 
+	export _BUILD_DIR="${_PROJECTS_DIR}/liferay-portal-ee"
+
 	lc_cd "${_PROJECTS_DIR}"/liferay-portal-ee
 
-	git checkout master &>/dev/null
+	git checkout master &> /dev/null
 
-	git fetch upstream --no-tags &>/dev/null
+	git fetch upstream --no-tags &> /dev/null
 
-	git merge upstream/master &>/dev/null
+	git merge upstream/master &> /dev/null
 
 	mkdir -p "${_PROJECTS_DIR}/liferay-portal-ee/release"
 }
 
 function tear_down {
+	unset LIFERAY_RELEASE_PRODUCT_NAME
 	unset _BUILD_DIR
 	unset _PRODUCT_VERSION
 	unset _RELEASE_ROOT_DIR
-	unset LIFERAY_RELEASE_PRODUCT_NAME
 
 	rm -r "${_PROJECTS_DIR}/liferay-portal-ee/release"
 
@@ -58,7 +59,11 @@ function tear_down {
 function test_generate_release_notes {
 	generate_release_notes
 
-	assert_equals "${LIFERAY_COMMON_EXIT_CODE_BAD}" $(grep -q "\-," release/release-notes.txt; echo $?) "${LIFERAY_COMMON_EXIT_CODE_OK}" $(grep -q "LPD-27038" release/release-notes.txt; echo $?)
+	assert_equals \
+		$(grep -q "\-," release/release-notes.txt; echo "${?}") \
+		"${LIFERAY_COMMON_EXIT_CODE_BAD}" \
+		$(grep -q "LPD-27038" release/release-notes.txt; echo "${?}") \
+		"${LIFERAY_COMMON_EXIT_CODE_OK}"
 }
 
 main
