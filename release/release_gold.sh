de451bea9993b4156e35a78d1194bc03e585b2ca
Pedro Malta <pedro.malta@liferay.com>
2024-11-08 14:20:38 -0300

    LPD-35947 Create prepare branch to commit from master function

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -80,6 +80,29 @@ function main {
 	lc_time_run add_patcher_project_version
 }
 
+function prepare_branch_to_commit_from_master {
+	lc_cd "${1}"
+
+	git fetch upstream master
+
+	git checkout master
+
+	git reset --hard upstream/master
+
+	git push --delete origin "${2}"
+
+	git branch --delete --force "${2}"
+
+	git checkout -b "${2}"
+
+	git push origin "${2}" --force
+
+	if [ "$(git rev-parse --abbrev-ref HEAD)" != "${2}" ]
+	then
+		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+	fi
+}
+
 function prepare_next_release_branch {
 	if [[ "${_PRODUCT_VERSION}" != *q* ]]
 	then
