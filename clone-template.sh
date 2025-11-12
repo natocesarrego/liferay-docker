4a08748e0c76e465fd04445235d24b0cebb14ec6
Renato Rego <renato.rego@liferay.com>
2025-11-12 14:38:23 -0300

    Make clear private functions

--- clone-template.sh
+++ clone-template.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-function clone_latest_commit {
-	git clone \
-		--branch=master \
-		--depth 1 \
-		--single-branch \
-		https://github.com/liferay/liferay-portal.git "${TEMP_DIR}"
-}
-
-function extract_template {
-	local template_origin="${TEMP_DIR}/modules/integrations/vercel/templates/${TEMPLATE}"
-
-	echo -e "extracting ${template_origin} to ${DESTINATION}"
-
-	if [ ! -d "${DESTINATION}" ]
-	then
-		mkdir --parents "${DESTINATION}"
-	fi
-
-	mv --verbose "${template_origin}" "${DESTINATION}"
-}
-
 function main {
 	TEMPLATE="${1}"
 
@@ -33,14 +12,35 @@ function main {
 
 	TEMP_DIR=$(mktemp -d)
 
-	clone_latest_commit
+	_clone_latest_commit
 
-	extract_template
+	_extract_template
 
-	make_destination_a_git_repo
+	_make_destination_a_git_repo
 }
 
-function make_destination_a_git_repo {
+function _clone_latest_commit {
+	git clone \
+		--branch=master \
+		--depth 1 \
+		--single-branch \
+		https://github.com/liferay/liferay-portal.git "${TEMP_DIR}"
+}
+
+function _extract_template {
+	local template_origin="${TEMP_DIR}/modules/integrations/vercel/templates/${TEMPLATE}"
+
+	echo -e "extracting ${template_origin} to ${DESTINATION}"
+
+	if [ ! -d "${DESTINATION}" ]
+	then
+		mkdir --parents "${DESTINATION}"
+	fi
+
+	mv --verbose "${template_origin}" "${DESTINATION}"
+}
+
+function _make_destination_a_git_repo {
 	cd "${DESTINATION}/${TEMPLATE}"	 && git init && git add . && git commit --message "chore: clone TEMPLATE"
 }
 
