11369634f4f767762c2517a828dc4e2422c69a81
Renato Rego <renato.rego@liferay.com>
2025-11-12 14:01:11 -0300

    Sort

--- clone-template.sh
+++ clone-template.sh
@@ -3,7 +3,7 @@
 function clone_latest_commit {
 	local temp_dir=${1}
 
-	git clone --single-branch --branch=master --depth 1 https://github.com/liferay/liferay-portal.git ${temp_dir}
+	git clone --branch=master --depth 1 --single-branch https://github.com/liferay/liferay-portal.git ${temp_dir}
 }
 
 function extract_template {
