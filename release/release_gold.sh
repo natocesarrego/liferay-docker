996fabbf3559112af0f563581839af065eb4d5a2
Renato Rego <renato.rego@liferay.com>
2026-01-05 13:23:13 -0300

    regenerate-releases-json

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -87,34 +87,34 @@ function main {
 
 	lc_time_run set_jdk_version_and_parameters
 
-	lc_time_run promote_packages
+	# lc_time_run promote_packages
 
-	lc_time_run tag_release
+	# lc_time_run tag_release
 
-	promote_boms xanadu
+	# promote_boms xanadu
 
-	lc_time_run generate_releases_json
+	lc_time_run generate_releases_json "regenerate"
 
-	lc_time_run reference_new_releases
+	# lc_time_run reference_new_releases
 
-	lc_time_run test_boms
+	# lc_time_run test_boms
 
-	lc_time_run update_salesforce_product_version
+	# lc_time_run update_salesforce_product_version
 
-	if [ -d "${_RELEASE_ROOT_DIR}/dev/projects" ]
-	then
-		lc_background_run clone_repository liferay-portal-ee
+	# if [ -d "${_RELEASE_ROOT_DIR}/dev/projects" ]
+	# then
+	# 	lc_background_run clone_repository liferay-portal-ee
 
-		lc_wait
-	fi
+	# 	lc_wait
+	# fi
 
-	lc_time_run clean_portal_repository
+	# lc_time_run clean_portal_repository
 
-	lc_time_run prepare_next_release_branch
+	# lc_time_run prepare_next_release_branch
 
-	lc_time_run add_patcher_project_version
+	# lc_time_run add_patcher_project_version
 
-	lc_time_run upload_to_docker_hub
+	# lc_time_run upload_to_docker_hub
 }
 
 function prepare_next_release_branch {
