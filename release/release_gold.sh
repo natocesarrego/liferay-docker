0af5346956692af22087d3676d858297dc058b26
Renato Rego <renato.rego@liferay.com>
2026-03-19 12:51:18 -0300

    Temporarily remove unnecessary steps from release gold

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -104,28 +104,28 @@ function main {
 
 	lc_time_run promote_packages
 
-	lc_time_run tag_release
+	# lc_time_run tag_release
 
-	promote_boms xanadu
+	# promote_boms xanadu
 
-	lc_time_run generate_releases_json
+	# lc_time_run generate_releases_json
 
-	lc_time_run reference_new_releases
+	# lc_time_run reference_new_releases
 
-	lc_time_run update_salesforce_product_version
+	# lc_time_run update_salesforce_product_version
 
-	if [ -d "${_RELEASE_ROOT_DIR}/dev/projects" ]
-	then
-		lc_background_run clone_repository liferay-portal-ee
+	# if [ -d "${_RELEASE_ROOT_DIR}/dev/projects" ]
+	# then
+	# 	lc_background_run clone_repository liferay-portal-ee
 
-		lc_wait
-	fi
+	# 	lc_wait
+	# fi
 
-	lc_time_run clean_portal_repository
+	# lc_time_run clean_portal_repository
 
-	lc_time_run prepare_next_release_branch
+	# lc_time_run prepare_next_release_branch
 
-	lc_time_run add_patcher_project_version
+	# lc_time_run add_patcher_project_version
 
 	lc_time_run upload_to_docker_hub
 
